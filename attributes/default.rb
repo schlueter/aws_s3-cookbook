b32d314c301f46f274781854a1ba785dd70e8a24
Schlueter <schlueter@bschlueter.com>
2014-06-04 22:12:23 -0400

    Add metadata and default attributes

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default[:amazon_s3][:access_key_id]     = nil
+default[:amazon_s3][:secret_access_key] = nil
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'amazon_s3'
+maintainer       'Schlueter'
+maintainer_email 'bs@bschlueter.com'
+license          'GPL v3'
+description      'Manipulate objects in Amazon s3'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
+supports         'ubuntu'
